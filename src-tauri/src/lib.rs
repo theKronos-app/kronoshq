e983d2f75e29e9b48ce9f3f49aee5aaf60425651
OmarGoubail <omarsayed223@gmail.com>
2025-01-08 14:50:10 +0200

    feat: add tauri sql

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,29 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri_plugin_sql::{Builder, Migration, MigrationKind};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+
+    let migrations = vec![
+        Migration {
+            version: 1,
+            description: "create notes table",
+            sql: "CREATE TABLE IF NOT EXISTS notes (
+                id TEXT PRIMARY KEY,
+                content TEXT,
+                created_at INTEGER,
+                modified_at INTEGER,
+                type TEXT,
+                tags TEXT,
+                properties JSON
+            )",
+            kind: MigrationKind::Up,
+        },
+    ];
     tauri::Builder::default()
+        .plugin(tauri_plugin_sql::Builder::default()
+                .add_migrations("sqlite:kronoshpere.db", migrations).build())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        // .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
