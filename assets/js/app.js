53f7edd3d9977098a4b17761bc9942831a64da05
OmarGoubail <omars.goubail@gmail.com>
2025-03-03 20:30:11 +0200

    added shadcn ui

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,7 +22,7 @@ import { Socket } from "phoenix"
 import { LiveSocket } from "phoenix_live_view"
 import topbar from "topbar" // instead of ../vendor/topbar
 import { getHooks } from "live_react";
-import components from "../react-components";
+import components from "../react";
 import "../css/app.css"
 
 
